af2542b443637fd5175cfe30093b6ec2b8b7649b
aaron <alurui1562@ieselcaminas.org>
2022-01-19 10:20:33 +0100

    terminado ORM

--- src/Controller/ContactoController.php
+++ src/Controller/ContactoController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contacto;
+use App\Entity\Provincia;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ContactoController extends AbstractController
 {
@@ -33,32 +36,143 @@ class ContactoController extends AbstractController
     // }
     
     /**
-     * @Route("/contacto/{codigo<\d+>?1}", name="ficha_contacto")
+     * @Route("/contacto/insertar", name="insertar_contacto")
      */
-    public function ficha($codigo): Response
+    public function insertar(ManagerRegistry $doctrine)
     {
-        $resultado = ($this->contactos[$codigo] ?? null);
-        if ($resultado) {
+        $entityManager = $doctrine->getManager();
+        foreach ($this->contactos as $c) {
+            $contacto = new Contacto();
+            $contacto->setNombre($c["nombre"]);
+            $contacto->setTelefono($c["telefono"]);
+            $contacto->setEmail($c["email"]);
+            $entityManager->persist($contacto);
+        }    
+        try {
+            $entityManager->flush();
+            return new Response("Contactos insertados");
+        } catch (\Exception $e) {
+            return new Response("Error insertano objetos");
+        }
+    }
+    /**
+     * @Route("/contacto/insertarConProvincia", name="insertar_con_provincia_contacto")
+     */
+    public function insertarConProvincia(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $provincia = new Provincia();
+    
+        $provincia->setNombre("Alicante");
+        $contacto = new Contacto();
+
+        $contacto->setNombre("Inserción de prueba con provincia");
+        $contacto->setTelefono("90220022");
+        $contacto->setEmail("insercion.de.prueba.provincia@contacto.es");
+        $contacto->setProvincia($provincia);
+        
+        $entityManager->persist($provincia);
+        $entityManager->persist($contacto);
+        $entityManager->flush();
+        return $this->render('ficha_contacto.html.twig', [
+            'contacto' => $contacto
+        ]);
+
+    }
+
+    /**
+     * @Route("/contacto/insertarSinProvincia", name="insertar_sin_provincia_contacto")
+     */
+    public function insertarSinProvincia(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $repositorio = $doctrine->getRepository(Provincia::class);
+    
+        $provincia = $repositorio->findOneBy(["nombre" => "Alicante"]);
+        $contacto = new Contacto();
+
+        $contacto->setNombre("Inserción de prueba sin provincia");
+        $contacto->setTelefono("90220022");
+        $contacto->setEmail("insercion.de.prueba.sin.provincia@contacto.es");
+        $contacto->setProvincia($provincia);
+        
+        $entityManager->persist($contacto);
+        $entityManager->flush();
+        return $this->render('ficha_contacto.html.twig', [
+            'contacto' => $contacto
+        ]);
+
+    }
+    /**
+     * @Route("/contacto/update/{id}/{nombre}", name="modificar_contacto")
+     */
+    public function update(ManagerRegistry $doctrine, $id, $nombre): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $repositorio = $doctrine->getRepository(Contacto::class);
+        $contacto = $repositorio->find($id);
+        if ($contacto) {
+            $contacto->setNombre($nombre);
+            try {
+                $entityManager->flush();
+                return $this->render('ficha_contacto.html.twig', [
+                    'contacto' => $contacto
+                ]);
+            } catch (\Exception $e) {
+                return new Response("Error insertando objetos");
+            }
+        }else {
             return $this->render('ficha_contacto.html.twig', [
-                'contacto' => $resultado
+                'conacto' => null
             ]);
         }
+    }
+
+    /**
+     * @Route("/contacto/delete/{id}", name="eliminar_contacto")
+     */
+    public function delete(ManagerRegistry $doctrine, $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $repositorio = $doctrine->getRepository(Contacto::class);
+        $contacto = $repositorio->find($id);
+        if ($contacto) {
+            try {
+                $entityManager->remove($contacto);
+                $entityManager->flush();
+                return new Response("Contacto eliminado");
+            } catch (\Exception $e) {
+                return new Response("Error eliminando objeto");
+            }
+        }else {
+            return $this->render('ficha_contacto.html.twig', [
+                'conacto' => null
+            ]);
+        }
+    }
+    /**
+     * @Route("/contacto/{codigo<\d+>?1}", name="ficha_contacto")
+     */
+    public function ficha(ManagerRegistry $doctrine, $codigo): Response
+    {
+        $repositorio = $doctrine->getRepository(Contacto::class);
+        $contacto = $repositorio->find($codigo);
+
+        return $this->render('ficha_contacto.html.twig', [
+            'contacto' => $contacto
+        ]);
     }
     
     /**
      * @Route("/contacto/buscar/{texto}", name="buscar_contacto")
      */
-    public function buscar($texto): Response
+    public function buscar(ManagerRegistry $doctrine, $texto): Response
     {
-        $resultados = array_filter($this->contactos,
-            function ($contacto) use ($texto)
-            {
-                return strpos($contacto["nombre"], $texto) !== FALSE;
-            }
-        );
-        
+        $repositorio = $doctrine->getRepository(Contacto::class);
+        $contactos = $repositorio->find($texto);
+
         return $this->render('lista_contactos.html.twig', [
-            'contactos' => $resultados
+            'contactos' => $contactos
         ]);
         
     }
